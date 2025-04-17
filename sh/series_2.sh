4b09f4d68c9f0f3b1a0f140fe1309ef5aebd336c
statsign <val44324@gmail.com>
2025-04-17 14:04:41 +0300

    Changes in sh scripts

--- sh/series_2.sh
+++ sh/series_2.sh
@@ -8,15 +8,15 @@ source venv/bin/activate
 
 PROFILES_JSON='[
   [
-    {"type": "gauss", "params": {"A": -6}, "label": "Gaussian (A=-6, k=-0.1)", "name": "pr4"},
-    {"type": "gauss", "params": {"A": 0}, "label": "Gaussian (A=0, k=-0.1) (linear)", "name": "pr5"},
-    {"type": "gauss", "params": {"A": 0, "k": -0.2}, "label": "Gaussian (A=0, k=-0.2) (linear)", "name": "pr6"}
+    {"type": "gauss", "params": {"A": -6}, "label": "Gaussian (A=-6, k=-0.1)", "name": "pr1"},
+    {"type": "gauss", "params": {"A": 0}, "label": "Gaussian (A=0, k=-0.1) (linear)", "name": "pr2"},
+    {"type": "gauss", "params": {"A": 0, "k": -0.2}, "label": "Gaussian (A=0, k=-0.2) (linear)", "name": "pr3"}
   ],
   [
-    {"type": "linear", "params": {"slope": -0.1}, "label": "Linear (slope=-0.1)", "name": "pr1"},
-    {"type": "linear", "params": {"slope": -0.07}, "label": "Linear (slope=-0.07)", "name": "pr2"},
-    {"type": "gauss", "params": {"A": -3}, "label": "Gaussian (A=-3)", "name": "pr7"}
-  ],
+    {"type": "linear", "params": {"slope": -0.1}, "label": "Linear (slope=-0.1)", "name": "pr4"},
+    {"type": "linear", "params": {"slope": -0.07}, "label": "Linear (slope=-0.07)", "name": "pr5"},
+    {"type": "gauss", "params": {"A": -3}, "label": "Gaussian (A=-3)", "name": "pr6"}
+  ]
 
 ]'
 
